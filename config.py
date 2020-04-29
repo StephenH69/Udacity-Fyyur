37d07eaf50b81849c7a4dfc19816eefadce6958f
Stephen <venus.stephenh@outlook.com>
2020-04-29 12:54:49 +0100

    Updated config file

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connect to the database
-SQLALCHEMY_DATABASE_URI = 'postgres://postgres:Riley250769?@127.0.0.1:5432/fyyur'
+SQLALCHEMY_DATABASE_URI = 'postgres://postgres:PASSWORD@127.0.0.1:5432/fyyur'
